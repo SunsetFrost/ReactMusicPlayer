1cd28e9ff8181f065c3e695ac882bc858898d730
sunset frost <710851944@qq.com>
2017-09-14 16:18:28 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports={
+    entry: './app/index.js',
+    output:{
+        path: './dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        resolve:{
+            extensions:['','.js','.json']
+        },        
+        loaders: [
+            {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              loader: "babel-loader",
+              query:
+                {
+                  presets:['react','es2015']
+                }
+            },
+            {
+                test: /\.json?$/,
+                loader: 'json'
+            },
+            {
+                test: /\.css$/,
+                loader: "style!css"
+            },
+            {
+                test: /\.less/,
+                loader: 'style-loader!css-loader!less-loader'
+            }
+        ]
+    }
+}
